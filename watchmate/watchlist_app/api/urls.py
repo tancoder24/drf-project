47ca79817eecc5854fcf7ba39bb4c2e6842e47fc
tanmayS2C <tanmay.g@survey2connect.com>
2023-04-26 13:48:47 +0530

    L 16

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from watchlist_app.api.views import movie_list, movie_detail
+from watchlist_app.api.views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
-    path('list/', movie_list, name="movie_list"),
-    path('detail/<int:pk>', movie_detail, name="movie_detail"),
+    path('list/', MovieListAV.as_view(), name="movie_list"),
+    path('detail/<int:pk>', MovieDetailAV.as_view(), name="movie_detail"),
 ]
