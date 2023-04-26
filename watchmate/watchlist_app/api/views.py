b085555afd6f3b93678f9f3dc86146155809b996
tanmayS2C <tanmay.g@survey2connect.com>
2023-04-26 13:03:06 +0530

    L 14

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -4,14 +4,40 @@ from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
-@api_view(["GET"])
+@api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
+
     movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+
+
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    
+    elif request.method == "PUT":
+        serializer = MovieSerializer(movie, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    
+    elif request.method == "DELETE":
+        movie.delete()
+        return Response()
+        
